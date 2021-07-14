83398f47b8d689bafa82cd280e1c90cca2d906cb
Nobreganj <57805973+Nobreganj@users.noreply.github.com>
2021-07-14 01:02:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
         hour = minute * 60,
         day = hour * 24;
 
-  let birthday = "Jul 28, 2021 01:02:00",
+  let birthday = "Jul 28, 2021 01:05:00",
       countDown = new Date(birthday).getTime(),
       x = setInterval(function() {    
 
